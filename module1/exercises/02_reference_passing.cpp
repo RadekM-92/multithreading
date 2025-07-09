29f3097b9ca0a681a0f48f9796e92eb93005b4b4
Radoslaw Marcinowski <rm92.git@gmail.com>
2025-07-09 15:07:26 +0200

    Added reference passing example

--- module1/exercises/02_reference_passing.cpp
+++ module1/exercises/02_reference_passing.cpp
@@ -5,12 +5,19 @@ using namespace std;
 void add10(int & a)
 {
     a += 10;
+    std::cout<<"Inside thread: " << a <<std::endl;
 }
 
 int main()
 {
     // run add10 function in a thread
     // pass 5 as an argument and read it's value
+    int sum = 5;
+    std::thread th(add10, std::ref(sum));
+    std::cout<<"Before thread join(): " << sum <<std::endl;
+    th.join();
+    std::cout<<"After thread join(): " << sum <<std::endl;
+
     return 0;
 }
 
